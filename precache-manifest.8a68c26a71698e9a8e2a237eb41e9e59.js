d87c55a001b81a3dc73d92110fabf04b20241750
Oleksandr Zdebskyi <technomaster@ukr.net>
2020-09-23 15:35:56 +0300

    Updates

---
+++ precache-manifest.8a68c26a71698e9a8e2a237eb41e9e59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6939fc486da81bb69aef0018f93f9353",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "d729f233a658c04eb209",
+    "url": "/goit-react-hw-01-components/static/css/main.942284fe.chunk.css"
+  },
+  {
+    "revision": "13a030a57f3648a9d35e",
+    "url": "/goit-react-hw-01-components/static/js/2.e5c56e05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.e5c56e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d729f233a658c04eb209",
+    "url": "/goit-react-hw-01-components/static/js/main.563841c2.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
